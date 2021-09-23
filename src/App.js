44ed1388fde98e18f02579fd6fcf72508aa6630e
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-23 15:26:46 +0300

    UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import SearchBar from './components/SearchBar';
+import BookData from './Data.json';
 
 function App() {
     return (
         <div className='App'>
-            <SearchBar />
+            <SearchBar placeholder='Enter a Book Name...' data={BookData} />
         </div>
     );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,42 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: 30,
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 300,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
+    divider: {
+        height: 28,
+        margin: 4,
+    },
+}));
+
 const SearchBar = ({ placeholder, data }) => {
+    const classes = useStyles();
+
     return (
-        <div className='search'>
-            <div className='searchInput'>
-                <input type='text' />
-                <div className='searchIcon'></div>
-            </div>
-            <div className='dataResult'></div>
-        </div>
+        <Paper component='form' className={classes.root}>
+            <InputBase
+                className={classes.input}
+                placeholder='Search Google Maps'
+                inputProps={{ 'aria-label': 'search google maps' }}
+            />
+
+            <SearchIcon />
+        </Paper>
     );
 };
-
 export default SearchBar;
